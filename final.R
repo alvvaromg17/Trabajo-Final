069352ff4372c57cf1ca69d3371f341a7f53e426
alvvaromg17 <alvvaromg17@gmail.com>
2023-05-27 22:56:36 +0200

    final

--- final.R
+++ final.R
@@ -45,6 +45,11 @@ modelo <- glm (forma ~ borde, data = ceramicas, family = binomial)
 summary (modelo)
 
 
+#Ahora, utilizamos la altura como variable dependiente
+
+modelo1 <- glm (forma ~ altura, data = ceramicas, family = binomial)
+summary(modelo1)
+
 #Con anova() analizamos la tabla de desviación.
 anova(modelo)
 
@@ -59,15 +64,11 @@ predicted.classes1
 
 
 
-
-#Ahora, utilizamos la altura como variable dependiente
-
-modelo1 <- glm (forma ~ altura, data = ceramicas, family = binomial)
-summary(modelo1)
-
+#Ahora, con la Altura
 
 confint(object = modelo1, level = 0.95)
 
+anova(modelo1, test ='Chisq')
 
 newdataaltura <- data.frame(altura)
 probabilities <- modelo %>% predict(newdataaltura, type = "response")
@@ -75,6 +76,7 @@ predicted.classes2 <- ifelse(probabilities > 0.5, "Lekythos", "Kylix")
 predicted.classes2
 
 
+#Regresión multiple
 
 modelo2 <- glm (forma ~ altura + borde, data = ceramicas, family = binomial)
 summary(modelo2)
@@ -87,173 +89,126 @@ probabilities <- modelo2 %>% predict(newdatamult, type = "response")
 predicted.classes2 <- ifelse(probabilities > 0.5, "Lekythos", "Kylix")
 predicted.classes2
 
-
+--
 modelo3 <- glm (forma ~ altura + borde + base, data = ceramicas, family = binomial)
 summary(modelo3)
 
 
+summary(modelo3)
+
+#Con coef () sacamos solamente los coeficientes.
 coef(modelo3)
-summary(modelo3)$coef
+
+
+anova(modelo3, test ='Chisq')
 
 
 newdatamult2 <- data.frame(altura + borde + base)
 probabilities <- modelo3 %>% predict(newdatamult2, type = "response")
-predicted.classes3 <- ifelse(probabilities > 0.5, "Lekythos", "Kylix")
+predicted.classes3 <- ifelse(probabilities > 0.5, "Kylix", "Lekythos")
 predicted.classes3
 
-ceramicas = as.data.frame(copas)
+#Técnica como predictora.
 
-is.data.frame(ceramicas)
-table(ceramicas)
-
-
-ceramicas$Forma=factor (ceramicas$Forma, levels = c('0', '1'), labels = c ("Kylix", "Castulo"))
-ceramicas$Tecnica=factor (ceramicas$Tecnica, levels = c('0', '1'), labels = c ("FR", "BN"))
+modelo_tec <- glm (tecnica ~ borde, data = ceramicas, family = binomial)
 
 
-View (ceramicas)
-
-freq.fr = table (ceramicas$Tecnica)
-View (freq.fr)
-
-x = ceramicas$Forma
-y = ceramicas$Tecnica
-
-datos = data.frame(x, y)
-View (datos)
-
-
-library(ggplot2)
-ggplot(data = ceramicas, aes(x = forma, y = borde)) +
-  geom_point(aes(color = as.factor(borde)), shape = 1) + 
-  stat_function(fun = function(x){predict(modelo,
-                                          newdata = data.frame(forma = x),
-                                          type = "response")}) +
-  theme_bw() +
-  labs(title = "Regresión logística",
-       y = "Probabilidad Altura") +
-  theme(legend.position = "none")
-
-
-
-
-
-
-
-ggplot(data = ceramicas, aes(x = forma, y = altura)) +
-  geom_point(aes(color = as.factor(altura)), shape = 1) + 
-  stat_function(fun = function(x){predict(modelo,
-                                          newdata = data.frame(forma = x),
-                                          type = "response")}) +
-  theme_bw() +
-  labs(title = "Regresión logística",
-       y = "Probabilidad Altura") +
-  theme(legend.position = "none")
-
-
-
-
-as.numeric(borde)
-
-plot(forma ~ borde, ceramicas, col = "darkblue",
-     main = "Modelo regresión logística",
-     ylab = "P(forma1|borde)",
-     xlab = "ceramicas", pch = "I")
-# type = "response" devuelve las predicciones en forma de probabilidad en lugar de en log_ODDs
-curve(predict(modelo, data.frame(borde = x), type = "response"),
-      col = "firebrick", lwd = 2.5, add = TRUE)
-
-
-
-
-plot(forma ~ altura, ceramicas, col = "darkblue",
-     main = "Modelo regresión logística",
-     ylab = "P(forma1|altura)",
-     xlab = "ceramicas", pch = "I")
-# type = "response" devuelve las predicciones en forma de probabilidad en lugar de en log_ODDs
-curve(predict(modelo1, data.frame(altura = x), type = "response"),
-      col = "firebrick", lwd = 2.5, add = TRUE)
-
-
-
-
+#Representaciones gráficas
 
 library(ggplot2)
 
-ggplot(data = ceramicas, aes(x = forma, y = borde, color = forma)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_jitter(width = 0.1) +
+#diagrama de caja y bigote
+
+ggplot(data = ceramicas, mapping = aes(x = forma, y = borde)) +
+  geom_boxplot(aes(color = as.factor (forma))) +
+  geom_point(aes(color = as.factor (forma))) +
   theme_bw() +
   theme(legend.position = "null")
 
 
+#Con ggplot ()
+
+ggplot(data = ceramicas, aes(x = borde, y = forma)) + 
+  geom_point(aes(color = as.factor(forma)), shape = "I", size = 3) +
+  stat_function(fun = function(x){predict(modelo,
+                                          newdata = data.frame(borde = x),
+                                          type = "response")}) +
+  theme_bw() +
+  labs(title = "Regresión logística",
+       y = "Probabilidad Forma (1 = Kylix, 0 = Lekythos") +
+  theme(legend.position = "none")
+
+#Con plot ()
+
+plot(forma ~ borde, ceramicas, col = "darkblue",
+     main = "Modelo regresión logística",
+     ylab = "Forma",
+     xlab = "Diámetro Borde", pch = "I")
+# type = "response" devuelve las predicciones en forma de probabilidad en lugar de en log_ODDs
+curve(predict(modelo, data.frame(borde = x), type = "response"),
+      col = "firebrick", lwd = 2.5, add = TRUE)
+
+#Forma y Altura
+ggplot(data = ceramicas, aes(x = altura, y = forma)) + 
+  geom_point(aes(color = as.factor(forma)), shape = "I", size = 3) +
+  stat_function(fun = function(x){predict(modelo1,
+                                          newdata = data.frame(altura = x),
+                                          type = "response")}) +
+  theme_bw() +
+  labs(title = "Regresión logística",
+       y = "Probabilidad Forma (1 = Kylix, 0 = Lekythos") +
+  theme(legend.position = "none")
 
 
 
+#Técnica y Borde
+ggplot(data = ceramicas, aes(x = borde, y = tecnica)) + 
+  geom_point(aes(color = as.factor(tecnica)), shape = "I", size = 3) +
+  stat_function(fun = function(x){predict(modelo_tec,
+                                          newdata = data.frame(borde = x),
+                                          type = "response")}) +
+  theme_bw() +
+  labs(title = "Regresión logística",
+       y = "Probabilidad Técnica (1 = Barniz Negro, 0 = Figurada") +
+  theme(legend.position = "none")
+
+#Forma con Borde y Altura
 
 
+ggplot(data = ceramicas, aes(x = altura + borde, y = forma)) + 
+  geom_point(aes(color = as.factor(forma)), shape = "I", size = 3) +
+  stat_function(fun = function(x){predict(modelo,
+                                          newdata = data.frame(borde = x),
+                                          type = "response")}) +
+  stat_function(fun = function(x){predict(modelo1,
+                                          newdata = data.frame(altura = x),
+                                          type = "response")}) +
+  theme_bw() +
+  labs(title = "Regresión logística múltiple",
+       y = "Probabilidad Forma (1 = Barniz Negro, 0 = Figurada") +
+  theme(legend.position = "none")
+
+#Diagrama de mosaico
+
+# Cálculo de la probabilidad del modelo.
+
+newdatafor <- predict(modelo, newdata = data.frame(forma), type = "response")
+
+# Vector de elementos “Lekythos”
+pred.modelo <- rep("Lekythos", length(newdatafor))
+
+pred.modelo [newdatafor > 0.5] <- "Kylix"
 
 
+# Matriz de confusión
+
+matriz <- table(pred.modelo, forma,
+                dnn = c("Forma", "predicciones"))
+
+matriz
+
+library(vcd)
+mosaic(matriz, shade = T, colorize = T, 
+       gp = gpar(fill = matrix(c("#99cc99", "#cc9999"), 2, 2)))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Grafica que muestra la diferencia entre la variable de origen 1 y 0 
-ggplot(data, aes(variablex, fill = factor(variabley))) + 
-  geom_histogram(binwidth = 1, position = 'dodge') + 
-  labs(title = 'Diferencia entre la variable de origen 1 y 0', 
-       x = 'Altura', 
-       y = 'Frecuencia')
-
-#Calculo de la precisión del modelo
-
-dif_residuos <- modelo$null.deviance - modelo$deviance
-
-# Grados libertad
-df <- modelo$df.null - modelo$df.residual
-
-# p-value
-p_value <- pchisq(q = dif_residuos,df = df, lower.tail = FALSE)
-
---------------------------
-  paste("Diferencia de residuos:", round(dif_residuos, 4))
-
-paste("Grados de libertad:", df)
-
-paste("p-value:", p_value)
----------------------------
-  #Comparación de clasificación predicha y observaciones
-  
-  library(vcd)
-
-predicciones <- ifelse(test = modelo_logistico$fitted.values > 0.5, yes = 1, no = 0)
-
-matriz_confusion <- table(variabley, predicciones,
-                          dnn = c("observaciones", "predicciones"))
-matriz_confusion
-
-#Predicciones y osbservaciones 
-mosaic(matriz_confusion, shade = T, colorize = T,
-       gp = gpar(fill = matrix(c("green3", "red2", "red2", "green3"), 2, 2)))
-
-#Conclusión 
-#Los resultados de la regresión logística muestran que la altura es un factor significativo para predecir 
-#el origen de los datos con un p-value de 0.000. Esto significa que a medida que aumenta la altura, es 
-#más probable que el origen sea 1. Además, la matriz de confusión muestra que el modelo es un buen predictor, 
-#ya que la mayoría de los registros fueron clasificados correctamente. Por lo tanto, se puede concluir que 
-#la altura es un predictor significativo para predecir el origen de los datos.
